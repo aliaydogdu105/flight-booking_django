502a2b13db6de1d3b0bd814a8550b144516368eb
aliaydogdu105 <aliaydogdu105@gmail.com>
2023-04-07 23:59:13 +0300

    feat: add flight-serializers

--- flight/serializers.py
+++ flight/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Flight
+from .models import Flight, Reservation, Passenger
 
 
 class FlightSerializer(serializers.ModelSerializer):
@@ -15,3 +15,38 @@ class FlightSerializer(serializers.ModelSerializer):
             "date_of_departure",
             "etd"
         )
+
+class PassengerSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = Passenger
+        fields = "__all__"
+
+class ReservationSerializer(serializers.ModelSerializer):
+
+    passenger = PassengerSerializers(many=True, required=True)
+    flight = serializers.StringRelatedField()
+    flight_id = serializers.IntegerField()
+    user = serializers.StringRelatedField()
+    
+    class Meta:
+        model = Reservation
+        fields = (
+            "id",
+            "flight",
+            "flight_id",
+            "user",
+            "passenger"
+        )
+    
+    def create(self, validated_data):
+        passenger_data = validated_data.pop("passenger")
+        validated_data["user_id"] = self.context["request"].user.id
+        reservation = Reservation.objects.create(**validated_data)
+        
+        for passenger in passenger_data:
+            pas = Passenger.objects.create(**passenger)
+            reservation.passenger.add(pas)
+        
+        reservation.save()
+        return reservation
--- flight/views.py
+++ flight/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import FlightSerializer
-from .models import Flight
+from .serializers import FlightSerializer, ReservationSerializer
+from .models import Flight, Reservation
 from rest_framework.permissions import IsAdminUser
 from .permissions import IsStafforReadOnly
 
@@ -10,3 +10,7 @@ class FlightView(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
     permission_classes = (IsStafforReadOnly,)
+
+class ReservationView(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
